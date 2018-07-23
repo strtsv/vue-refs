617c5bab869e2d15442a1259930fabc4ad7bb3cf
jsusilo <enr@bk.ru>
2018-07-23 17:30:14 +0500

    click the button to set input focus

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello Vue!</h1>
+    <input type='text' ref='name'/>
+    <button @click='focus'>Focus input</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  methods: {
+    focus () {
+      console.log('here will be focus', this.$refs.name)
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
