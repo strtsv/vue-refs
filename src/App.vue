43858e5eaf6bcf4020815199ed01ee31b749b247
jsusilo <enr@bk.ru>
2020-04-28 22:30:39 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <div>
-    <h1>Hello Vue!</h1>
-    <input type='text' ref='name'/>
-    <button @click='focus'>Focus input</button>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  methods: {
-    focus () {
-      console.log('here will be focus', this.$refs.name)
-      this.$refs.name.focus()
-    }
+  name: 'App',
+  components: {
+    HelloWorld
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.config.productionTip = false
+
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  render: function (h) { return h(App) },
+}).$mount('#app')
